99e9661f9ba9c634448b5701991cc059ee342f22
_itsy4sh <itsy4sh.dev@gmail.com>
2025-03-29 16:04:47 +0530

    nvm

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -1,32 +1,16 @@
-import { AppSidebar } from "@/components/dashboard/AppSidebar/app-sidebar";
-import { SiteHeader } from "@/components/dashboard/AppSidebar/site-header";
 import { Box } from "@/components/dashboard/Main/header/box";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 export default function Page() {
 	return (
-		<SidebarProvider
-			style={
-				{
-					"--sidebar-width": "calc(var(--spacing) * 72)",
-					"--header-height": "calc(var(--spacing) * 12)",
-				} as React.CSSProperties
-			}
-		>
-			<AppSidebar variant='inset' />
-			<SidebarInset className='bg-background'>
-				<SiteHeader />
-				<main className='h-full bg-background grid grid-rows-[3fr_2fr_4fr] rows-[header_main_footer] '>
-					<div className='row-in-header grid grid-cols-[2fr_4fr] cols-[left_right]'>
-						<div className='cols-in-left p-4'>
-							<Box />
-						</div>
-						<div className='col-in-Right'></div>
-					</div>
-					<div className='row-in-main'> </div>
-					<div className='row-in-footer'></div>
-				</main>
-			</SidebarInset>
-		</SidebarProvider>
+		<main className='h-full bg-background grid grid-rows-[3fr_2fr_4fr] rows-[header_main_footer] '>
+			<div className='row-in-header grid grid-cols-[2fr_4fr] cols-[left_right]'>
+				<div className='cols-in-left p-4'>
+					<Box />
+				</div>
+				<div className='col-in-Right'></div>
+			</div>
+			<div className='row-in-main'> </div>
+			<div className='row-in-footer'></div>
+		</main>
 	);
 }
--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,5 +1,7 @@
 import { Metadata } from "next";
-
+import { AppSidebar } from "@/components/dashboard/AppSidebar/app-sidebar";
+import { SiteHeader } from "@/components/dashboard/AppSidebar/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 export const metadata: Metadata = {
 	title: {
 		template: "Home",
@@ -12,5 +14,20 @@ export default async function HomeLayout({
 }: {
 	children: React.ReactNode;
 }) {
-	return <div>{children}</div>;
+	return (
+		<SidebarProvider
+			style={
+				{
+					"--sidebar-width": "calc(var(--spacing) * 72)",
+					"--header-height": "calc(var(--spacing) * 12)",
+				} as React.CSSProperties
+			}
+		>
+			<AppSidebar variant='inset' />
+			<SidebarInset className='bg-background'>
+				<SiteHeader />
+				{children}
+			</SidebarInset>
+		</SidebarProvider>
+	);
 }
