5a189b42fda0224bd5bbbec63caa4f44d5b8cece
_itsy4sh <itsy4sh.dev@gmail.com>
2025-04-04 22:13:54 +0530

    redirect removed

--- components/dashboard/AppSidebar/nav-main.tsx
+++ components/dashboard/AppSidebar/nav-main.tsx
@@ -9,7 +9,7 @@ import {
 	SidebarMenuButton,
 	SidebarMenuItem,
 } from "@/components/ui/sidebar";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export function NavMain({
 	items,
@@ -20,6 +20,7 @@ export function NavMain({
 		icon?: Icon;
 	}[];
 }) {
+	const router = useRouter();
 	return (
 		<SidebarGroup>
 			<SidebarGroupContent className='mt -10 font-cabinet-medium flex flex-col gap-2'>
@@ -32,7 +33,7 @@ export function NavMain({
 							<SidebarMenuButton
 								tooltip={item.title}
 								onClick={() => {
-									redirect(item.url);
+									router.push(item.url);
 								}}
 								className='space-x-4 py-8 px-6  hover:border-l-2 hover:border-primary/90 hover:bg-primary/5'
 							>
