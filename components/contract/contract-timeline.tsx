e4094dbee2ed7c17243fea81f3d2daeb454c9011
_itsy4sh <itsy4sh.dev@gmail.com>
2025-04-04 21:00:10 +0530

    timline dotstatus

--- components/contract/contract-timeline.tsx
+++ components/contract/contract-timeline.tsx
@@ -11,30 +11,59 @@ import { TimelineType } from "@/types/types";
 interface TimelineProps {
 	data: TimelineType[];
 }
+
 const ContractTimeline: React.FC<TimelineProps> = ({ data }) => {
+	// Find the index of the last "done" item
+	const lastDoneIndex = data.reduce((lastIndex, item, index) => {
+		return item.status === "done" ? index : lastIndex;
+	}, -1);
+
 	return (
 		<>
 			<Timeline>
-				{data.map((item, index) => (
-					<TimelineItem key={item.id}>
-						<TimelineHeading
-							status={item.status === "done" ? "done" : "default"}
-							className='text-xl'
-						>
-							{item.date} - {item.status}
-						</TimelineHeading>
-						<TimelineDot
-							status={item.status === "done" ? "done" : "default"}
-						/>
-						{index !== data.length - 1 && <TimelineLine done />}
-						<TimelineContent
-							status={item.status === "done" ? "done" : "default"}
-							className='text-[0.8em]'
-						>
-							{item.title || `Payment: $${item.payment}`}
-						</TimelineContent>
-					</TimelineItem>
-				))}
+				{data.map((item, index) => {
+					// Determine dot status based on requirements
+					let dotStatus:
+						| "done"
+						| "current"
+						| "default"
+						| "error"
+						| "custom"
+						| null
+						| undefined = "default";
+
+					if (item.status === "done") {
+						dotStatus = "done";
+					} else if (item.status === "hold") {
+						dotStatus = "error";
+					} else if (
+						item.status === "pending" &&
+						index === lastDoneIndex + 1
+					) {
+						dotStatus = "current";
+					}
+
+					return (
+						<TimelineItem key={item.id}>
+							<TimelineHeading
+								status={item.status === "done" ? "done" : "default"}
+								className='text-xl'
+							>
+								{item.date} - {item.status}
+							</TimelineHeading>
+							<TimelineDot status={dotStatus} />
+							{index !== data.length - 1 && (
+								<TimelineLine done={item.status === "done"} />
+							)}
+							<TimelineContent
+								status={item.status === "done" ? "done" : "default"}
+								className='text-[0.8em]'
+							>
+								{item.title || `Payment: $${item.payment}`}
+							</TimelineContent>
+						</TimelineItem>
+					);
+				})}
 			</Timeline>
 		</>
 	);
