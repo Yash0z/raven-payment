2100cc58a4e6a8076019e1a1a3bda223e2e015b8
_itsy4sh <itsy4sh.dev@gmail.com>
2025-04-10 19:23:24 +0530

    ...

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	output: "export", // Required for Cloudflare Pages static exports
+	// This is needed if you're using the App Router
+	images: {
+		unoptimized: true,
+	},
+	// We need to disable this since we're not using Pages Functions
+
+	appDir: true,
 };
 
 export default nextConfig;
