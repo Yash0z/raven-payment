3b45a277a024a19d40e17cb6314c150d9730c46f
_itsy4sh <itsy4sh.dev@gmail.com>
2025-04-11 20:45:57 +0530

    Details page

--- components/contract/my-contracDetails.tsx
+++ components/contract/my-contracDetails.tsx
@@ -1,6 +1,16 @@
 import { TimelineType } from "@/types/types";
 import ContractTimeline from "./contract-timeline";
 import { Badge } from "@/components/ui/badge";
+import {
+	Dialog,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
+
+import { Button } from "../ui/button";
+import { Link } from "lucide-react";
 interface DataProps {
 	data: {
 		hexID: string;
@@ -17,7 +27,7 @@ interface DataProps {
 const MyContractDetails: React.FC<DataProps> = ({ data }) => {
 	return (
 		<>
-			<main className='w-full h-full border-4 p-3 grid grid-rows-[1fr_6fr] gap-3'>
+			<main className='w-full h-full  p-3 grid grid-rows-[1fr_6fr] gap-3'>
 				<div className='border p-5 flex justify-between'>
 					<div>
 						<span className='flex gap-5 items-center'>
@@ -46,14 +56,37 @@ const MyContractDetails: React.FC<DataProps> = ({ data }) => {
 					</div>
 				</div>
 
-				<div className='flex justify-between '>
-					<div className='border-2 w-[49%] p-5'>
+				<div className='flex flex-col md:flex-row md:justify-between gap-3'>
+					<div className='border-2 md:w-[59%] p-5'>
 						<h1 className='text-2xl font-satoshi-regular pb-6 text-muted-foreground'>
 							Timeline
 						</h1>
 						<ContractTimeline data={data?.timeline} />
 					</div>
-					<div className='border-2 w-[49%]'>h1 </div>
+					<div className='border-2 md:w-[59%] p-5 '>
+						<Dialog>
+							<DialogHeader className='text-2xl font-satoshi-regular pb-6 text-muted-foreground'>
+								Agreement Details
+							</DialogHeader>
+							<DialogTrigger asChild>
+								<Button variant='outline'>
+									<span>
+										<Link />
+									</span>
+									<span>File</span>
+								</Button>
+							</DialogTrigger>
+							<DialogContent className='text-start border-secondary bg-background overflow-y-auto text-xl h-[700px] w-[600px] '>
+								<DialogTitle className='text-muted-foreground'>
+									TERMS AND CONDITIONS
+								</DialogTitle>
+								{data.contractAgreement
+									? data.contractAgreement
+									: "No agreement data available"}
+							</DialogContent>
+						</Dialog>
+						<div className='mt-5'></div>
+					</div>
 				</div>
 			</main>
 		</>
